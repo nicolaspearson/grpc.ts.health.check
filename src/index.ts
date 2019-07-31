8508939de3aca51f64efa013c2d9777621745cc9
Nic Pearson <npearson@minddoc.de>
2019-07-31 21:43:26 +0200

    Added Watch Function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import * as grpc from 'grpc';
 import GrpcBoom from 'grpc-boom';
 
-import { HealthClient, HealthService } from './proto/health_grpc_pb';
+import { HealthClient, HealthService, IHealthServer } from './proto/health_grpc_pb';
 import { HealthCheckRequest, HealthCheckResponse } from './proto/health_pb';
 
-class GrpcHealthCheck {
-	constructor(private statusMap: { [key: string]: number }) {
+class GrpcHealthCheck implements IHealthServer {
+	private watchStatusMap: { [key: string]: HealthCheckResponse.ServingStatus } = {};
+	private watchErrorMap: { [key: string]: Error } = {};
+
+	constructor(private statusMap: { [key: string]: HealthCheckResponse.ServingStatus }) {
 		// Empty
 	}
 
-	private setStatus(service: string, status: number): void {
+	private setStatus(service: string, status: HealthCheckResponse.ServingStatus): void {
 		this.statusMap[service] = status;
+	}
+
+	private sendStatusResponse(
+		call: grpc.ServerWriteableStream<HealthCheckRequest>,
+		status: HealthCheckResponse.ServingStatus,
+		callback?: (error: Error | null | undefined) => void
+	): void {
+		// Send the status to the client
+		const response = new HealthCheckResponse();
+		response.setStatus(status);
+		call.write(response, callback);
 	}
 
 	public check(
@@ -29,18 +43,44 @@ class GrpcHealthCheck {
 	}
 
 	public watch(
-		call: grpc.ServerUnaryCall<HealthCheckRequest>,
-		callback: grpc.sendUnaryData<HealthCheckResponse>
+		call: grpc.ServerWriteableStream<HealthCheckRequest>
 	): void {
 		const service: string = call.request.getService();
-		// TODO: Complete implementation
-		const status: number = HealthCheckResponse.ServingStatus.UNKNOWN;
-		this.setStatus(service, status);
-		// Check the status of the service
-		// Send the initial response
-		const response: HealthCheckResponse = new HealthCheckResponse();
-		response.setStatus(status);
-		callback(null, response);
+		// Updated status is used for getting service status updates.
+		let updatedStatus = HealthCheckResponse.ServingStatus.SERVING;
+		if (!this.statusMap[service]) {
+			// Set the initial status
+			updatedStatus = HealthCheckResponse.ServingStatus.SERVICE_UNKNOWN;
+			this.setStatus(service, updatedStatus);
+			this.sendStatusResponse(call, updatedStatus);
+		} else {
+			updatedStatus = this.statusMap[service];
+		}
+
+		this.watchStatusMap[service] = updatedStatus;
+		let lastStatus = -1;
+		if (!this.watchErrorMap[service]) {
+			// tslint:disable no-console
+			console.log('Next Tick');
+			const callback = () => {
+				lastStatus = this.statusMap[service];
+				if (lastStatus !== updatedStatus) {
+					console.log('Sending Status: ' + updatedStatus);
+					// Status has changed
+					this.setStatus(service, updatedStatus);
+					this.sendStatusResponse(call, updatedStatus, (error: Error | null | undefined) => {
+						if (error) {
+							// Terminate stream on next tick
+							this.watchErrorMap[service] = error;
+						}
+					});
+				}
+			};
+			process.nextTick(callback);
+		} else {
+			// Terminate the stream
+			call.end(this.watchErrorMap[service]);
+		}
 	}
 }
 
